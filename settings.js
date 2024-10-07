837fae6ec9a531fb2da3d08888782590de63f36f
Pabaya257 <dineshremixhub@gmail.com>
2024-10-07 22:57:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+if (fs.existsSync('config.env')) require('dotenv').config({ path: 'VAJIRA-MD=DQVmWSIC#o8UAoS1OidarCjFTGtka6PiziQx_X8aMVW3MCXSz8f8' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? '94741093187' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94741093187' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
